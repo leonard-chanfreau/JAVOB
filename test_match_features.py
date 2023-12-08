6b966b18fd8298aeb5951c859a398a8901cf95bc
Elliot Irving <elliotirving@Elliots-MacBook-Pro.local>
2023-12-08 16:40:11 +0100

    Storing to database works but not clean. To work on in group.

--- test_match_features.py
+++ test_match_features.py
@@ -60,9 +60,11 @@ if __name__ == '__main__':
             # pruned_descriptors = new_3d_descriptors[mask_recover[:, 0] == 1] # OLD
 
             # TESTING (briefly examined output, seems to work)
+            new_3d_keypoints = [query_features["keypoints"][match.queryIdx] for match in matches2d2d]
             new_3d_descriptors = [query_features["descriptors"][match.queryIdx] for match in matches2d2d]
-            mask_recover_int = mask_recover[:, 0].astype(int)
-            pruned_descriptors = np.array(new_3d_descriptors)[mask_recover_int == 1] # float32 dtype
+            mask_recover = mask_recover[:, 0].astype(int)
+            pruned_keypoints = np.array(new_3d_keypoints)[mask_recover == 1] # float32 dtype
+            pruned_descriptors = np.array(new_3d_descriptors)[mask_recover == 1] # float32 dtype
             # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -91,34 +93,42 @@ if __name__ == '__main__':
             matches3d2d = vo.match_features("3d2d", num_previous_descriptors=num_previous_descriptors)
 
             object_points = np.array([vo.world_points_3d[-num_previous_descriptors:,:3][match.trainIdx] for match in matches3d2d])
-            query_points = np.array([query_features["keypoints"][match.queryIdx].pt for match in matches3d2d])
+            query_points = np.array([query_features["keypoints"][match.queryIdx] for match in matches3d2d])
+            query_points_uv = np.array([query_features["keypoints"][match.queryIdx].pt for match in matches3d2d]) #extracted img pts
 
             success, rvec_C_W, t_C_W, inliers = cv2.solvePnPRansac(
-                object_points, query_points, cameraMatrix=vo.K, distCoeffs=np.zeros((4,1)))
+                object_points, query_points_uv, cameraMatrix=vo.K, distCoeffs=np.zeros((4,1)))
             if not success:
                 raise RuntimeError("RANSAC is not able to fit the model")
             
             # Handle inliers
-            num_p3p_inliers = len(inliers) # number inliers (not used)
+            num_p3p_inliers = len(inliers) # tracks number 3d-2d inliers (currently not used)
             inlier_3d_mask = inliers[:, 0].astype(int)
             # recover_matches = matches3d2d[inlier_3d_mask]
-            recover_inlier_2d_points = query_points[inlier_3d_mask] # selects 2d points for plotting
+            recover_inlier_2d_points = query_points[inlier_3d_mask] # selects 2d points for plotting (not _uv)
             recover_inlier_3d_points = object_points[inlier_3d_mask] # for PLOTTING 3D points on image. We might need to store these for the visualizer
             
             R_C_W, _ = cv2.Rodrigues(rvec_C_W)
             
             # Write pose to self.poses
+            build_M = np.hstack((R_C_W,t_C_W))
             if pose_exists is True:
-                vo.poses = np.dstack((vo.poses, np.hstack((R_C_W,t_C_W)))) # TODO pose database FAILS HERE
+                vo.poses = np.vstack((vo.poses,np.reshape(build_M,(1,3,4)))) # currently vstacking poses. May change to dstack
             else:
                 raise ValueError("P3P attempting to write pose to uninitialized pose database. 8pt pose should have been written first")
 
             # Visualize
             projected_3D_points = cv2.projectPoints(objectPoints=recover_inlier_3d_points, 
-                                                    rvec=vo.poses[-1,:3,:3], tvec=vo.poses[-1,:3,4],
-                                                    cameraMatrix=vo.K) # reprojects database 3d points into image plane
-            img3 = cv2.drawMatches(query_img, recover_inlier_2d_points, prev_img, recover_inlier_3d_points,
-                               matches3d2d, None, flags=2)
+                                                    rvec=vo.poses[-1,:3,:3], tvec=vo.poses[-1,:3,-1],
+                                                    cameraMatrix=vo.K, distCoeffs=None) # reprojects database 3d points into image plane
+            
+            # extract projectPoints output to get u,v. Can probably fix this with different datastructures?
+            projected_3D_points = np.hstack((projected_3D_points[0][:,:,0], projected_3D_points[0][:,:,1])) 
+
+            # NOTE/TODO fails, can't read projected_3D_points since not cv2.Keypoint dtype
+            img3 = cv2.drawMatches(query_img, recover_inlier_2d_points, 
+                                   prev_img, projected_3D_points,
+                                   matches3d2d, None, flags=2)
 
         cv2.imshow('2d2d SFM then 3d2d Matching', img3)
         cv2.waitKey(1000)
